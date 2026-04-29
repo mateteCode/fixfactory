9c7e3e1e9106d10b87f8ee77978920c076300606
Matías Lorenzo <matete@gmail.com>
2026-04-29 06:01:18 -0300

    feat: add RBAC and JWT security middlewares

---
+++ apps/api/src/middlewares/auth.middleware.ts
@@ -0,0 +1,22 @@
+import type { Request, Response, NextFunction } from "express";
+import jwt from "jsonwebtoken";
+
+export const authenticate = (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  const token = req.headers.authorization?.split(" ")[1]; // Espera "Bearer TOKEN"
+
+  if (!token) {
+    return res.status(401).json({ message: "Acceso denegado. No hay token." });
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET || "secret_key");
+    (req as any).user = decoded; // Guardamos los datos del usuario en la petición
+    next();
+  } catch (error) {
+    res.status(401).json({ message: "Token no válido o expirado." });
+  }
+};
--- apps/api/src/routes/dashboard.routes.ts
+++ apps/api/src/routes/dashboard.routes.ts
@@ -1,8 +1,16 @@
 import { Router } from "express";
 import { getGeneralStats } from "../controllers/dashboard.controller.js";
+import { authenticate } from "../middlewares/auth.middleware.js";
+import { authorize } from "../middlewares/role.middleware.js";
+import { UserRole } from "../models/User.js";
 
 const router = Router();
 
-router.get("/stats", getGeneralStats);
+router.get(
+  "/stats",
+  authenticate,
+  authorize([UserRole.ADMIN, UserRole.GERENTE]),
+  getGeneralStats,
+);
 
 export default router;
